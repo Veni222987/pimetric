9175f67a2937923efb9a5a6a00e5782176e56abb
Veni <veni222987@gmail.com>
2024-06-04 23:38:23 +0800

    feat(exporter): 规定导出的样式

--- exporter.go
+++ exporter.go
@@ -6,21 +6,29 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/Veni222987/pimetric/store"
+	"github.com/Veni222987/pimetric/counter"
+	"github.com/Veni222987/pimetric/gauge"
+	"github.com/Veni222987/pimetric/historgram"
+	"github.com/Veni222987/pimetric/pimstore"
 )
+
+type Metricx struct {
+	AppName      string                           `json:"app_name"`
+	CounterMap   map[string]*counter.Counter      `json:"counter_map"`
+	GaugeMap     map[string]*gauge.Gauge          `json:"gauge_map"`
+	HistogramMap map[string]*historgram.Histogram `json:"histogram_map"`
+}
 
 // GenHandler 指定appName生成handler，可以注册到已有的端口上
 func GenHandler(appName string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		resMap := map[string]interface{}{
-			appName: map[string]interface{}{
-				store.COUNTER_KEY:   store.CounterMap,
-				store.GAUGE_KEY:     store.GaugeMap,
-				store.HISTOGRAM_KEY: store.HistogramMap,
-			},
-		}
-		resBytes, err := json.Marshal(resMap)
+		resBytes, err := json.Marshal(Metricx{
+			AppName:      appName,
+			CounterMap:   pimstore.CounterMap,
+			GaugeMap:     pimstore.GaugeMap,
+			HistogramMap: pimstore.HistogramMap,
+		})
 		if err != nil {
 			w.Write([]byte(err.Error()))
 			return
